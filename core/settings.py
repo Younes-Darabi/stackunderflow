a7ccfaaa67b6bd0376ccdb42572ae65a0ecd03a0
Younes Darabi <younes.darabi@outlook.com>
2026-03-28 02:25:08 +0100

    add QuestionTest

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'stackunderflow.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'stackunderflow.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
@@ -129,9 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework.authentication.TokenAuthentication',
-    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
--- forum_app/tests.py
+++ forum_app/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+from forum_app.api.serializers import QuestionSerializer
+from forum_app.models import Question
+
+
+class LikeTest(APITestCase):
+
+    def test_get_like(self):
+        url = reverse('like-list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class QuestionTest(APITestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword'
+        )
+        self.question = Question.objects.create(
+            title='test Question',
+            content='Test Content',
+            author=self.user,
+            category='frontend'
+        )
+        # self.client = APIClient()
+        # self.client.login(
+        #     username='testuser',
+        #     password='testpassword'
+        # )
+        self.token = Token.objects.create(user=self.user)
+        self.client = APIClient()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+
+
+    def test_post_list_question(self):
+        url = reverse('question-list')
+        data = {
+            'title': 'Question1',
+            'content': '1Content',
+            'author': self.user.id,
+            'category': 'frontend'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_detail_get_question(self):
+        url = reverse('question-detail', kwargs={'pk': self.question.id})
+        response = self.client.get(url)
+        expected_data = QuestionSerializer(self.question).data
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertDictEqual(response.data, expected_data)
+        self.assertJSONEqual(response.content, expected_data)
+        self.assertContains(response, 'title')
+        self.assertEqual(Question.objects.count(), 1)
+        self.assertEqual(Question.objects.get().author, self.user)
